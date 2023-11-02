d012814d9a5d2ff7154eb8f7b5f04ed33f5d7385
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-11-01 17:13:28 -0700

    Initial Commit

---
+++ In-Class Activity Pro IPhone Ch5 Understanding Concurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-Class Activity Pro IPhone Ch5 Understanding Concurrency
+//
+//  Created by Student Account on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-Class Activity Pro IPhone Ch5 Understanding Concurrency/In_Class_Activity_Pro_IPhone_Ch5_Understanding_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch5_Understanding_ConcurrencyApp.swift
+//  In-Class Activity Pro IPhone Ch5 Understanding Concurrency
+//
+//  Created by Student Account on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch5_Understanding_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
